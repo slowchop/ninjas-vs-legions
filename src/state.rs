4cbd735f125280e8274978fbbe888d61d7cf1b05
gak <gak@gak0.com>
2020-06-27 16:02:46 +1000

    fitted pixel font

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,6 @@
 use ggez::*;
 use ggez::graphics::{Color, Drawable, DrawParam};
+use mint::Vector2;
 use shipyard::World;
 use crate::SCREEN_WIDTH;
 
@@ -38,15 +39,22 @@ impl ggez::event::EventHandler for State {
 
         image.draw(ctx, DrawParam::new());
 
-        let mut scoreboard_text = graphics::Text::new("hi jart");
-        scoreboard_text.set_font(graphics::Font::default(), graphics::Scale::uniform(30.0));
+        let font = graphics::Font::new(ctx, "/fonts/Open Sans Px/OpenSansPX.ttf")?;
+
+        let mut scoreboard_text = graphics::Text::new("hi jart what's going on? huh? pew pew pew 1234567890");
+        scoreboard_text.set_font(font, graphics::Scale::uniform(70.0));
 
         let coords = [
             SCREEN_WIDTH / 2.0 - scoreboard_text.width(ctx) as f32 / 2.0,
             90.0,
         ];
 
-        let params = graphics::DrawParam::default().dest(coords).color(Color::new(0., 0., 0., 1.));
+        let coords: [f32; 2] = [0.0, 10.];
+
+        let params = graphics::DrawParam::default()
+            .dest(coords)
+            .color(Color::new(0., 0., 0., 1.0))
+            .scale(Vector2 { x: 0.2, y: 0.2 });
         graphics::draw(ctx, &scoreboard_text, params).expect("error drawing scoreboard text");
 
         graphics::present(ctx).expect("error presenting");
